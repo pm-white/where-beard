ba376b82de580bce582dd4666174744db1e248e8
pm-white <p.mrcr.w@gmail.com>
2025-02-01 15:25:45 -0500

    contiuing exploring data

--- get_data.py
+++ get_data.py
@@ -5,10 +5,24 @@ url = "https://www.jamesbeard.org/blog/the-2025-james-beard-award-semifinalists#
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
 
-states = ["MA", "RI", "MA", "NH", "VT"]
+states = ["MA", "RI", "ME", "NH", "VT"]
+
+finalists = {}
 
 for finalist in soup.find_all(["h4", "li"]):
+    # print(finalist.name)
     state = finalist.text.split(",")[-1].replace(")", "").strip()
-    # print(state)
-    if state in states:
-        print(finalist.text)
+    if finalist.name == "h4":
+        new_category = finalist.text
+        finalists[new_category] = []
+        # print(finalist.text)
+    elif state in states:
+        finalists[new_category].append(finalist.text)
+        # print("\t" + finalist.text)
+
+for cat, finalists in finalists.items():
+    if finalists:
+        print(cat, "-------", sep="\n")
+        for finalist in finalists:
+            print(" ", finalist)
+        print()
