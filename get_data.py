85e3cb02ee0d3c899c3e04ffb312320a12ae0b35
pm-white <p.mrcr.w@gmail.com>
2025-02-01 21:32:17 -0500

    refactor data wrangling. add google geocoding.

--- get_data.py
+++ get_data.py
@@ -1,28 +1,75 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+import googlemaps
 
-url = "https://www.jamesbeard.org/blog/the-2025-james-beard-award-semifinalists#"
-r = requests.get(url)
+YEAR = "2025"
+URL = "https://www.jamesbeard.org/blog/the-2025-james-beard-award-semifinalists#"
+
+with open("api_key.txt", "r") as f:
+    api_key = f.read()
+
+gmaps = googlemaps.Client(key=api_key)
+
+r = requests.get(URL)
 soup = BeautifulSoup(r.content, "html.parser")
 
-states = ["MA", "RI", "ME", "NH", "VT"]
+finalists = []
 
-finalists = {}
 
-for finalist in soup.find_all(["h4", "li"]):
-    # print(finalist.name)
-    state = finalist.text.split(",")[-1].replace(")", "").strip()
+def get_lat_lon(place: str) -> tuple[float, float]:
+    """
+    Returns a tuple of lat, lon from the google geocoding
+    api. Assumes the first result is the correct one.
+    """
+    geocode_result = gmaps.geocode(place)[0]
+    return (
+        geocode_result["geometry"]["location"]["lat"],
+        geocode_result["geometry"]["location"]["lng"],
+    )
+
+
+for finalist in soup.find_all(["h4", "li"])[:5]:
     if finalist.name == "h4":
-        new_category = finalist.text
-        finalists[new_category] = []
-        # print(finalist.text)
-    elif state in states:
-        finalists[new_category].append(finalist.text)
-        # print("\t" + finalist.text)
+        # award category
+        new_category = re.sub(r"\s+(p|P)resented.*", "", finalist.text.strip())
+    else:
+        # clean awardee info list
+        finalist_info = finalist.text.replace(" \xa0", "").split(",")
+        finalist_info = [x.strip() for x in finalist_info]
 
-for cat, finalists in finalists.items():
-    if finalists:
-        print(cat, "-------", sep="\n")
-        for finalist in finalists:
-            print(" ", finalist)
-        print()
+        num_items = len(finalist_info)
+        # no person listed in awardee info
+        if num_items == 3:
+            restaurant, city, state = finalist_info
+            person = ""
+        # info includes people
+        elif len(finalist_info) == 4:
+            person, restaurant, city, state = finalist_info
+
+        if num_items > 4:
+            # skip best restaurateur entries with multiple restaurants
+            pass
+        else:
+            # get coordinates
+            # place = restaurant + ", " + city + ", " + state + ", " + "USA"
+            # lat, lon = get_lat_lon(place)
+
+            # add structured info to list
+            finalists.append(
+                {
+                    "restaurant": restaurant,
+                    "year": YEAR,
+                    "category": new_category,
+                    "city": city,
+                    "state": state,
+                    "person": person,
+                    # "lat": lat,
+                    # "lon": lon,
+                }
+            )
+
+for i in finalists:
+    for k, v in i.items():
+        print(k, v)
+    print()
