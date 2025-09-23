43d86f88e2f1f59013b47c018edc19e951c2faaa
pm-white <philmwhite@pm.me>
2025-09-22 21:14:19 -0400

    get map working

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,7 +1,16 @@
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+
 function Map() {
   return (
     <>
-      <p>I'm the map!</p>
+      <MapContainer center={[40.76, -73.97]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          subdomains={"abcd"}
+          maxZoom={20}
+        />
+      </MapContainer>
     </>
   );
 }
