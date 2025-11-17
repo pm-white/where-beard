b28f9eec27549c65fbcde5b6992f5f20cf0a1c41
pm-white <philmwhite@pm.me>
2025-11-16 20:36:09 -0500

    fix mobile layout

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar({ years, year, setYear, categories, setCategory, category }) {
   return (
     <>
       <h1>James Beard Award Semifinalists</h1>
-      <p>
+      <p id="hide-mobile">
         This map shows semifinalist for the James Beard foundation's annual
         awards. Use the dropdown menus below to filter for an award year or a
         category.
