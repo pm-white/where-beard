824764a0ba955e3380b55318c7f9fa30b51d7b5d
pm-white <philmwhite@pm.me>
2026-09-23 21:52:08 -0400

    add carto api key to map component

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -8,7 +8,7 @@ function Map({ points, awards }) {
       <MapContainer center={[40.76, -73.97]} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url={`https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=${process.env.CARTO_API_KEY}`}
           subdomains={"abcd"}
           maxZoom={20}
         />
